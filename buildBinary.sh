0131fb136595f58f0324f785ed29d7ff1ae3ac13
jbenguira <joseph@appdrag.com>
2021-11-03 20:49:35 +0100

    Updated binary to Node 16

--- buildBinary.sh
+++ buildBinary.sh
@@ -17,7 +17,7 @@ rm -rf /tmp/ws-monitoring/helm/
 rm -rf /tmp/ws-monitoring/binaries/
 
 mkdir -p ./binaries;
-rm ./binaries/cloudgate_linux_x64_83.node
+rm ./binaries/cloudgate_linux_x64_93.node
 rm ./binaries/ws-monitoring
 rm ./binaries/ws-monitoring-linux.tar.gz
 
@@ -25,14 +25,14 @@ cd ./binaries;
 
 #package cloudgate as binaries for win/osx/linux
 #pkg /tmp/cloudgate/ --options max_old_space_size=4096 --targets node14-linux-x64,node14-win-x64,node14-macos-x64;
-pkg /tmp/ws-monitoring/ --options max_old_space_size=4096 --targets node14-linux-x64;
+pkg /tmp/ws-monitoring/ --options max_old_space_size=4096 --targets node16-linux-x64;
 
-#Copy cloudgate binaries for Node 14
+#Copy cloudgate binaries for Node 13
 #cp ../bin/cloudgate_win32_x64_83.node .
 #cp ../bin/cloudgate_darwin_x64_83.node .
-cp ../node_modules/@elestio/cloudgate/bin/cloudgate_linux_x64_83.node .
+cp ../node_modules/@elestio/cloudgate/bin/cloudgate_linux_x64_93.node .
 
 #create tar.gz
 #tar -czvf cloudgate-win.tar.gz cloudgate-win.exe cloudgate_win32_x64_83.node
 #tar -czvf cloudgate-osx.tar.gz cloudgate-macos cloudgate_darwin_x64_83.node
-tar -czvf ws-monitoring-linux.tar.gz ws-monitoring cloudgate_linux_x64_83.node
+tar -czvf ws-monitoring-linux.tar.gz ws-monitoring cloudgate_linux_x64_93.node
